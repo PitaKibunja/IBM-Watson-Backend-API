6578a5094cd8ffbf7363ff80298195edf3517172
dapintechnologies <dapintech@gmail.com>
2021-08-26 21:55:19 +0300

    Adding Watson configuration to app

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ require('dotenv').config()
 // 2: allow parsing
 app.use(express.json())
 // 2:Create a server and run it.
+const port=process.env.PORT||5000
+app.listen(port,()=>{
+    console.log("Server listening on port",port)
+})
---
+++ watson.js
@@ -0,0 +1,6 @@
+// 1.import depedencies
+
+// 2.Create instance of assstant
+// 3.Route to handle sesion tokens
+// 4.handle messages
+// 5.export routes
